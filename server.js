88ec0f14d839b27a564c871aee98275f45e84a91
student-ragini <kriragini2728@gmail.com>
2026-06-13 21:16:11 +0530

    debug cloudinary

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
     api_key: process.env.CLOUDINARY_CLIENT_API,
     api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
-})
+});
+console.log("Cloud Name:", process.env.CLOUDINARY_CLIENT_NAME);
+console.log("API Key:", process.env.CLOUDINARY_CLIENT_API);
+console.log("API Secret:", process.env.CLOUDINARY_CLIENT_SECRET ? "FOUND" : "NOT FOUND");
 
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Server running on port ${process.env.PORT}`);
-})
+});
